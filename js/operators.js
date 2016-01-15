5cbd79be468b543e337765abe39a6615ce25c070
dong hoon kim <donghoonkim1226@gmail.com>
2016-01-14 22:07:09 -0500

    Added arithmetic and comparison operators.

---
+++ js/operators.js
@@ -0,0 +1,25 @@
+// Comparison operators
+var myName = prompt("What's your name?");
+	if(myName.toLowerCase() === "donghoon") {
+		alert("Welcome Donghoon");
+
+		var bigNumber = 1234;
+		var testNumber = prompt("Pick a number");
+
+		if(testNumber > bigNumber) {
+			alert("You win!");
+		} else {
+			alert("You lose!")
+		}
+
+	} else {
+		alert("You are not welcome");
+	}
+
+// Arithmetic operators
+
+// var sideA = 3;
+// var sideB = 4;
+
+// var sideC = Math.sqrt(sideA * sideA + sideB * sideB);
+// console.log("sideC", sideC);
